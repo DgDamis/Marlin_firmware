76ec5128e99a024cb6ab460a6182f4e0469b1557
Damis <adam.smehyl@seznam.cz>
2021-06-30 18:19:02 +0200

    SKR 2 - ver. 4.3 firmware - fan tweaks

--- Marlin-SV01/Marlin/Configuration_adv.h
+++ Marlin-SV01/Marlin/Configuration_adv.h
@@ -488,7 +488,7 @@
 //#define FAN_KICKSTART_TIME 100
 
 // Some coolers may require a non-zero "off" state.
-#define FAN_OFF_PWM  1
+//#define FAN_OFF_PWM  1
 
 /**
  * PWM Fan Scaling
@@ -503,7 +503,7 @@
  * Define one or both of these to override the default 0-255 range.
  */
 //#define FAN_MIN_PWM 50
-//#define FAN_MAX_PWM 128
+#define FAN_MAX_PWM 128
 
 /**
  * FAST PWM FAN Settings
